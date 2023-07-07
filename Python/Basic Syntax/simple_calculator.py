33af1af655be3c41abe249b848e4e4a61cbdae9e
EnzoAblis <112990234+EnzoAblis@users.noreply.github.com>
2023-07-07 13:15:15 -0500

    initial commit

---
+++ Python/Basic Syntax/simple_calculator.py
@@ -0,0 +1,12 @@
+first_number = int(input("First Number: "))
+second_number = int(input("Second Number: "))
+operator = input("Operation: ")
+
+if operator == "+":
+    print(first_number + second_number)
+elif operator == "-":
+    print(first_number - second_number)
+elif operator == "*":
+    print(first_number * second_number)
+elif operator == "/":
+    print(first_number / second_number)
